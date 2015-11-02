c5e6c69c5f434a077638b1912b0c982f92339d2e
JakeyPedz <j.willisfleming@gmail.com>
2015-11-02 14:05:16 +0000

    Set up devise so that users can log in and log out

--- db/migrate/20151102114927_devise_create_users.rb
+++ db/migrate/20151102114927_devise_create_users.rb
@@ -30,6 +30,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :username
+      t.text :image
+      t.text :tagline
 
       t.timestamps null: false
     end
