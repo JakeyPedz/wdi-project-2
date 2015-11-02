d0d241d063e07ee201b52016b19957178634dfe3
JakeyPedz <j.willisfleming@gmail.com>
2015-11-02 18:25:31 +0000

    More emjoi's added

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,49 @@ cat30 = Category.create!(name: "🐗")     # such a bore(!)
 cat31 = Category.create!(name: "🐽")     # greedy/ smelly
 cat32 = Category.create!(name: "🐘")     # elephant
 cat33 = Category.create!(name: "🐪")     # camel
+cat34 = Category.create!(name: "🐰")     # rabbit/ easter
+cat35 = Category.create!(name: "🐔")     # chicken
+cat36 = Category.create!(name: "🐣")     # birth/ hatching chicken
+cat37 = Category.create!(name: "🐸")     # frog
+cat38 = Category.create!(name: "🐍")     # snake
+cat39 = Category.create!(name: "🐳")     # whale
+cat40 = Category.create!(name: "🐬")     # dolphin
+cat41 = Category.create!(name: "🐟")     # fish
+cat42 = Category.create!(name: "🐜")     # ant
+cat43 = Category.create!(name: "🐝")     # bee
+cat44 = Category.create!(name: "🌺")     # flower
+cat45 = Category.create!(name: "💐")     # bouquet
+cat46 = Category.create!(name: "🌹")     # rose
+cat47 = Category.create!(name: "🌴")     # palm tree/ paradise
+cat48 = Category.create!(name: "🌲")     # tree
+cat49 = Category.create!(name: "🌳")     # bushy tree
+cat50 = Category.create!(name: "🌵")     # cactus
+cat51 = Category.create!(name: "🌿")     # herb
+cat52 = Category.create!(name: "🍀")     # good luck/ 4 leaf clover 
+cat53 = Category.create!(name: "🌻")     # sunflower
+cat54 = Category.create!(name: "🌱")     # seedling/ growth
+cat55 = Category.create!(name: "🍂")     # Autumn
+cat56 = Category.create!(name: "🌍")     # global
+cat57 = Category.create!(name: "🌊 ")    # waves/ wavey/ surfing
+cat58 = Category.create!(name: "🌕")     # full moon
+cat59 = Category.create!(name: "🌙")     # moon
+cat60 = Category.create!(name: "☀️")     # sunny
+cat61 = Category.create!(name: "☁️")     # cloud/ morbid/ depressing  
+cat62 = Category.create!(name: "🌞")     # smiling sun
+cat63 = Category.create!(name: "☂")     # umbrella/ raining
+cat64 = Category.create!(name: "❄️")      # snow       
+cat65 = Category.create!(name: "🌠")     # shooting star
+cat66 = Category.create!(name: "🔥")     # fire
+cat67 = Category.create!(name: "⚡")     # lightening 
+cat68 = Category.create!(name: "⭐")     # star
+cat69 = Category.create!(name: "🍇")     # grapes
+cat70 = Category.create!(name: "🍋")     # lemon
+cat71 = Category.create!(name: "🍉")     # watermelon
+cat72 = Category.create!(name: "🍌")     # banana
+cat73 = Category.create!(name: "🍊")     # orange/ tangerine
+
+
+
 
 
 
