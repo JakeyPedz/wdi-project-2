1a84e9bf566382b8a9b632cbb9ca42d70caa54e3
JakeyPedz <j.willisfleming@gmail.com>
2015-11-03 09:53:18 +0000

    Got rid of one of the flash messages and enabled one to be able to search by category and by location

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -69,6 +69,6 @@ class StoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_params
-      params.require(:story).permit(:title, :content, :user_id, locations_ids: [], categories_ids: [])
+      params.require(:story).permit(:title, :content, :user_id, location_ids:[], category_ids:[])
     end
 end
